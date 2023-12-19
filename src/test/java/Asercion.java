a95a2136f7367fb1277cddff29af3b0bd58b4579
miguelMart23 <138571518+miguelMart23@users.noreply.github.com>
2023-12-19 09:38:01 +0100

    asertions validateURL, validateText

--- src/test/java/Asercion.java
+++ src/test/java/Asercion.java
@@ -1,13 +1,23 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 public class Asercion extends Main {
     
     public static void validarElemento(String xPath) {
-        WebElement element = driver.findElement(By.xpath(xPath));
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath)));
         Assert.assertTrue(element.isDisplayed());
+    }
+
+    public static void validarURL(String url) {
+        Assert.assertEquals(url, Main.driver.getCurrentUrl());
+    }
+
+    public static void validarTexto(String xPath, String textoAValidar) {
+        String texto = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath))).getText();
+        Assert.assertEquals(texto, textoAValidar);
     }
 
 }
